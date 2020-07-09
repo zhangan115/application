273182e6d126d64412ef16bb9e5cfacab2839d84
zhangan <zhangan115@gmail.com>
2020-07-09 17:56:11 +0800

    更新工单详情

---
+++ application/WorkProgressButtomCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkProgressButtomCell.swift
+//  application
+//
+//  Created by sitech on 2020/7/9.
+//  Copyright © 2020 Sitop. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import PGActionSheet
+import RealmSwift
+
+class WorkProgressButtomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.backgroundColor = UIColor.white
+    }
+    
+    func setModel(workModel:WorkModel){
+          
+    }
+}
